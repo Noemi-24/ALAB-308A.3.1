d5efbd428dcaf3ed22f04ddf4ad2601650eab63c
Noemi Roldan <noemi.roldan@peopleshores.com>
2026-02-12 10:41:24 -0500

    Db number return correctly

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 // Importing database functions. DO NOT MODIFY THIS LINE.
 import { central, db1, db2, db3, vault } from "./databases.js";
 
-function getUserData(id) {
+async function getUserData(id) {
   const dbs = {
     db1: db1,
     db2: db2,
     db3: db3
   };
+
+  const returnedDB = await central(id);    
+    console.log(returnedDB);
+
+    Promise.all([dbs[returnedDB], vault(id)])
+      .then(([result1, result2]) => {
+      console.log(result1, result2);
+    });
+    // const dbData = await (id);
+    // console.log(dbData);
 }
+
+
+getUserData(5);
