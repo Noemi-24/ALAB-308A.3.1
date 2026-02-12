a9b394637df47d26fe36dc4f937323f8cc3544c9
Noemi Roldan <noemi.roldan@peopleshores.com>
2026-02-12 11:47:21 -0500

    Validation completed

--- index.js
+++ index.js
@@ -8,16 +8,61 @@ async function getUserData(id) {
     db3: db3
   };
 
-  const returnedDB = await central(id);    
-    console.log(returnedDB);
+  try {
+    const returnedDB = await central(id);   
 
-    Promise.all([dbs[returnedDB], vault(id)])
+    Promise.all([dbs[returnedDB](id), vault(id)])
       .then(([result1, result2]) => {
-      console.log(result1, result2);
+
+        const userData = {
+          id: id,
+          name: result2.name,
+          username: result1.username,
+          email: result2.email,
+          address: {
+            street: result2.address.street,
+            suite: result2.address.suite,
+            city: result2.address.city,
+            zipcode: result2.address.zipcode,
+            geo:{
+              lat: result2.address.geo.lat,
+              lng: result2.address.geo.lng
+            }
+          },
+          phone: result2.phone,
+          website: result1.website,
+          company: {
+            name: result1.company.name,
+            catchPhrase: result1.company.catchPhrase,
+            bs: result1.company.bs    
+          }
+      };
+
+      console.log(userData);
+
     });
-    // const dbData = await (id);
-    // console.log(dbData);
+  } catch (error) {
+    console.error("An error occurred:", error.message); 
+  }
+  
 }
 
-
+// ==== Valid Numbers ====
+getUserData(1);
+getUserData(2);
+getUserData(3);
+getUserData(4);
 getUserData(5);
+getUserData(6);
+getUserData(7);
+getUserData(8);
+getUserData(9);
+getUserData(10);
+
+// ==== Invalid Numbers ====
+getUserData(0);
+getUserData(11);
+
+// ==== Invalid data types ====
+getUserData('1');
+getUserData(true);
